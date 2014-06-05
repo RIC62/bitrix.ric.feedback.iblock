e0293bf42b1067f6d5a8f861f20b75c87e7447ed
user <zek0072@gmail.com>
2014-06-05 11:15:38 +0400

    Добавление файлов в репозиторий

---
+++ lang/en/.parameters.php
@@ -0,0 +1,18 @@
+<?
+$MESS ['MFP_CAPTCHA'] = "Use CAPTCHA for Unauthorized Visitors";
+$MESS ['MFP_OK_MESSAGE'] = "Text To Show Upon Sending";
+$MESS ['MFP_OK_TEXT'] = "Thank you. Your message is now being processed.";
+$MESS ['MFP_EMAIL_TO'] = "Destination E-mail Address";
+$MESS ['MFP_REQUIRED_FIELDS'] = "Required fields";
+$MESS ['MFP_ALL_REQ'] = "(all non-required)";
+$MESS ['MFP_NAME'] = "Name";
+$MESS ['MFP_MESSAGE'] = "Message";
+$MESS ['MFP_EMAIL_TEMPLATES'] = "E-mail Templates";
+$MESS ['SETTINGS_IBLOCK'] = "Configure the infoblock";
+$MESS ["IBLOCK_SECTION_ID"] = "Section ID";
+$MESS ["IBLOCK_SECTION_CODE"] = "Section code";
+$MESS ['MFP_IBLOCK_NAME_TEXT'] = "Capture of message in block";
+$MESS ['MFP_IBLOCK_NAME_DEFAULT'] = "Feedback";
+$MESS ["T_IBLOCK_DESC_LIST_ID"] = "Information block code";
+$MESS ["T_IBLOCK_DESC_LIST_TYPE"] = "Type of information block (used for verification only)";
+?>
---
+++ lang/ru/.description.php
@@ -0,0 +1,7 @@
+<?
+$MESS ['MAIN_FEEDBACK_COMPONENT_NAME'] = "Форма обратной связи (расширенная)";
+$MESS ['MAIN_FEEDBACK_COMPONENT_DESCR'] = "Форма для отправки сообщения с сайта на E-mail (расширенная)<br />
+Для подключения к инфоблоку в нем требуются поля: <br />
+<b>'AUTHOR', 'AUTHOR_EMAIL', 'TEXT' и 'CUSTOM_#№ поля по порядку#'</b><br />
+ Не обязательным является список 'STATUS'";
+?>
